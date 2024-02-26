70568fb1f90291527e05bc8e71c9b928be7d0c45
HttpMarco <mirco.lindenau@gmx.de>
2024-02-26 21:46:47 +0100

    Complete service rest module

--- manager/src/main/java/net/nextcluster/manager/rest/ServiceRestService.java
+++ manager/src/main/java/net/nextcluster/manager/rest/ServiceRestService.java
@@ -2,12 +2,12 @@ package net.nextcluster.manager.rest;
 
 import net.nextcluster.driver.NextCluster;
 import net.nextcluster.driver.resource.service.ClusterService;
+import net.nextcluster.driver.resource.service.ServiceInformation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController("/service")
@@ -20,8 +20,34 @@ public final class ServiceRestService {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @GetMapping
+    public ResponseEntity<List<ServiceInformation>> readServices() {
+        return ResponseEntity.ofNullable(NextCluster.instance().serviceProvider().services().stream().map(ClusterService::information).toList());
+    }
+
+    @GetMapping
+    public ResponseEntity<ServiceInformation> readService(@RequestParam String id) {
+        return ResponseEntity.ofNullable(NextCluster.instance().serviceProvider().service(id).map(ClusterService::information).orElseGet(null));
+    }
+
     @PostMapping
-    public ResponseEntity<List<ClusterService>> readServices() {
-        return ResponseEntity.ofNullable(NextCluster.instance().serviceProvider().services());
+    public ResponseEntity<?> shutdownService(@RequestParam String id) {
+        var service = NextCluster.instance().serviceProvider().service(id);
+        if (service.isPresent()) {
+            service.get().shutdown();
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @PostMapping
+    public ResponseEntity<?> executeService(@RequestParam String id, @RequestParam String command) {
+        var service = NextCluster.instance().serviceProvider().service(id);
+        if (service.isPresent()) {
+            service.get().execute(command);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.noContent().build();
     }
 }
