83f979b207175c156c23d177f4b5e00a54370a48
Mirco Lindenau <51542098+HttpMarco@users.noreply.github.com>
2024-02-28 11:28:59 +0100

    Update install-cluster.sh

--- scripts/install-cluster.sh
+++ scripts/install-cluster.sh
@@ -41,7 +41,7 @@ else
    	fi
 	sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-	if [[ $ID == "debian"]]; then
+	if [[ $ID == "debian" ]]; then
 		echo \
 		  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
 		  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
